949c44739aa0be501a7da886737adf9535336a95
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-06 17:34:06 -0400

    rps first pass

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python
 
 import sys
+rps = [['rock'], ['paper'], ['scissors']]
+
+def getCartesianProduct(A,B):
+  prod = []
+  a_counter = 0
+  for a in A: # a ['r'] || O(3^n)
+    a_counter += 1
+    for b in B: # b ['r'] || O(3) since B is always `rps`
+      ordered_list = a + b
+      prod.append(ordered_list)
+  return prod
 
 def rock_paper_scissors(n):
-  pass 
+  counter = 1
+  if n <= 0:
+    return [[]]
 
+  res = rps
+  while counter != n: # O(n)
+    res = getCartesianProduct(res, rps)
+    counter += 1
+   
+  return res
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
